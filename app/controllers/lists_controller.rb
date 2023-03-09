f9fdc2aac74d8b4d05e4037bef049ac8ab6da80d
rmPablo1 <pablorubiiimonzo@gmail.com>
2023-03-09 21:49:32 +0100

    doing the show page for the cards

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = Bookmark.where(list_id: params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(set_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_params
+    params.require(:list).permit(:name)
+  end
+end
