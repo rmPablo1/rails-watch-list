0787272530bd7c7c28d13b92fc27b9ed325ee186
rmPablo1 <pablorubiiimonzo@gmail.com>
2023-03-09 12:27:08 +0100

    models finished

---
+++ db/migrate/20230309110503_change_name_column_to_bookmarks.rb
@@ -0,0 +1,6 @@
+class ChangeNameColumnToBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :bookmarks, :movies_id, :movie_id
+    rename_column :bookmarks, :lists_id, :list_id
+  end
+end
